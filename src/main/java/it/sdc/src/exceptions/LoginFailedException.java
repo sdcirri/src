2ea409dabecd5292f305d2103acc4b008e35dc6e
Sebastiano Deodati <sebastiano.deodati@etravelpartner.it>
2026-06-16 17:05:43 +0200

    better status code

--- src/main/java/it/sdc/src/exceptions/LoginFailedException.java
+++ src/main/java/it/sdc/src/exceptions/LoginFailedException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class LoginFailedException extends BusinessException {
     public LoginFailedException(String message) {
-        super(message, HttpStatus.FORBIDDEN);
+        super(message, HttpStatus.UNAUTHORIZED);
     }
 }
