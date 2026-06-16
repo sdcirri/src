cfa9201a776867dc71237d6ba698e37f3fdf97ed
Sebastiano Deodati <sebastiano.deodati@etravelpartner.it>
2026-06-16 17:52:27 +0200

    clean up unnecessary superclass

--- src/main/java/it/sdc/src/auth/AccessTokenIntrospector.java
+++ src/main/java/it/sdc/src/auth/AccessTokenIntrospector.java
@@ -2,11 +2,12 @@ package it.sdc.src.auth;
 
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class AccessTokenIntrospector extends BaseIntrospector {
+public class AccessTokenIntrospector implements OpaqueTokenIntrospector {
     private final TokenIntrospectionCache cache;
 
     @Override
