87708000e2232db23e9e875b707397c44bb95d6f
sdcirri <sebadeod@proton.me>
2026-06-20 13:20:35 +0200

    validation

--- src/main/java/it/sdc/src/config/AuthProperties.java
+++ src/main/java/it/sdc/src/config/AuthProperties.java
@@ -1,13 +1,16 @@
 package it.sdc.src.config;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "auth")
 @Getter
 @Setter
+@Validated
 public class AuthProperties {
-    private Long accessTokenValiditySeconds;
-    private Long refreshTokenValiditySeconds;
+    private @Positive long accessTokenValiditySeconds;
+    private @Positive long refreshTokenValiditySeconds;
 }
