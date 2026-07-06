7345f5c695888b9c9d9c338e35a031477b6f8d20
sdcirri <sebadeod@proton.me>
2026-07-06 20:31:51 +0200

    fix UUID generation

--- src/main/java/it/sdc/src/db/entities/UserSessionDB.java
+++ src/main/java/it/sdc/src/db/entities/UserSessionDB.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 @Builder
 public class UserSessionDB {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false, unique = true)
