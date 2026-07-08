97338ae565a145f4587ef2036115c6bb4c808bce
sdcirri <sebadeod@proton.me>
2026-07-08 09:49:40 +0200

    Token introspection unit tests

--- src/test/java/it/sdc/src/auth/TokenIntrospectionCacheTest.java
+++ src/test/java/it/sdc/src/auth/TokenIntrospectionCacheTest.java
@@ -5,6 +5,7 @@ import it.sdc.src.db.repositories.UserSessionDBRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.introspection.BadOpaqueTokenException;
 
 import java.security.SecureRandom;
 import java.util.Base64;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 import static it.sdc.src.test.fixtures.BearerAuthFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -44,9 +46,8 @@ public class TokenIntrospectionCacheTest {
     void introspectAccessToken_shouldReturnValidUserPrincipalOnValidAccessToken() {
         UserSessionDB userSession = mockSession(mockUserWithId(passwordEncoder));
         when(userSessionRepository.findByAccessToken(userSession.getAccessToken())).thenReturn(Optional.of(userSession));
-        String accessToken = ENCODER.encodeToString(userSession.getAccessToken());
 
-        UserPrincipal userPrincipal = cache.introspectAccessToken(accessToken);
+        UserPrincipal userPrincipal = cache.introspectAccessToken(ENCODER.encodeToString(userSession.getAccessToken()));
         assertThat(principalRefersToUser(userPrincipal, userSession)).isTrue();
     }
 
@@ -64,43 +65,62 @@ public class TokenIntrospectionCacheTest {
 
     @Test
     void introspectAccessToken_shouldRejectBadTokens() {
+        byte[] random = "random".getBytes();
 
+        when(userSessionRepository.findByAccessToken(random)).thenReturn(Optional.empty());
+        assertThatThrownBy(
+                () -> cache.introspectAccessToken(ENCODER.encodeToString(random))
+        ).isInstanceOf(BadOpaqueTokenException.class);
     }
 
     @Test
     void introspectAccessToken_shouldRejectExpiredTokens() {
+        UserSessionDB userSession = mockSessionWithExpiredAccessToken(mockUserWithId(passwordEncoder));
+        when(userSessionRepository.findByAccessToken(userSession.getAccessToken())).thenReturn(Optional.of(userSession));
 
+        assertThatThrownBy(
+                () -> cache.introspectAccessToken(ENCODER.encodeToString(userSession.getAccessToken()))
+        ).isInstanceOf(BadOpaqueTokenException.class);
     }
 
     @Test
-    void introspectRefreshToken_shouldReturnValidUserPrincipalOnValidAccessToken() {
+    void introspectRefreshToken_shouldReturnValidUserPrincipalOnValidRefreshToken() {
         UserSessionDB userSession = mockSession(mockUserWithId(passwordEncoder));
-        when(userSessionRepository.findByAccessToken(userSession.getAccessToken())).thenReturn(Optional.of(userSession));
-        String accessToken = ENCODER.encodeToString(userSession.getAccessToken());
+        when(userSessionRepository.findByRefreshToken(userSession.getRefreshToken())).thenReturn(Optional.of(userSession));
 
-        UserPrincipal userPrincipal = cache.introspectAccessToken(accessToken);
+        UserPrincipal userPrincipal = cache.introspectRefreshToken(ENCODER.encodeToString(userSession.getRefreshToken()));
         assertThat(principalRefersToUser(userPrincipal, userSession)).isTrue();
     }
 
     @Test
     void introspectRefreshToken_shouldCacheTokens() {
         UserSessionDB userSession = mockSession(mockUserWithId(passwordEncoder));
-        when(userSessionRepository.findByAccessToken(userSession.getAccessToken())).thenReturn(Optional.of(userSession));
-        String accessToken = ENCODER.encodeToString(userSession.getAccessToken());
+        when(userSessionRepository.findByRefreshToken(userSession.getRefreshToken())).thenReturn(Optional.of(userSession));
+        String refreshToken = ENCODER.encodeToString(userSession.getRefreshToken());
 
-        cache.introspectAccessToken(accessToken);
-        verify(userSessionRepository, times(1)).findByAccessToken(userSession.getAccessToken());
-        cache.introspectAccessToken(accessToken);
-        verify(userSessionRepository, times(1)).findByAccessToken(userSession.getAccessToken());
+        cache.introspectRefreshToken(refreshToken);
+        verify(userSessionRepository, times(1)).findByRefreshToken(userSession.getRefreshToken());
+        cache.introspectRefreshToken(refreshToken);
+        verify(userSessionRepository, times(1)).findByRefreshToken(userSession.getRefreshToken());
     }
 
     @Test
     void introspectRefreshToken_shouldRejectBadTokens() {
+        byte[] random = "random".getBytes();
 
+        when(userSessionRepository.findByRefreshToken(random)).thenReturn(Optional.empty());
+        assertThatThrownBy(
+                () -> cache.introspectRefreshToken(ENCODER.encodeToString(random))
+        ).isInstanceOf(BadOpaqueTokenException.class);
     }
 
     @Test
     void introspectRefreshToken_shouldRejectExpiredTokens() {
+        UserSessionDB userSession = mockSessionWithExpiredRefreshToken(mockUserWithId(passwordEncoder));
+        when(userSessionRepository.findByRefreshToken(userSession.getRefreshToken())).thenReturn(Optional.of(userSession));
 
+        assertThatThrownBy(
+                () -> cache.introspectRefreshToken(ENCODER.encodeToString(userSession.getRefreshToken()))
+        ).isInstanceOf(BadOpaqueTokenException.class);
     }
 }
