96e09e8eb6424896fdc43a786fe1b9f24a5689c4
sdcirri <sebadeod@proton.me>
2026-07-11 13:29:41 +0200

    size constraints

--- src/main/java/it/sdc/src/dto/requests/MessageRequest.java
+++ src/main/java/it/sdc/src/dto/requests/MessageRequest.java
@@ -2,8 +2,11 @@ package it.sdc.src.dto.requests;
 
 import it.sdc.src.validation.Base64String;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record MessageRequest(
-        @NotNull @Base64String String messageData,
-        @NotNull @Base64String String messageIV
+        // Allow messages of at most 20 MB (20971520 bytes become 27962028 when encoded)
+        @NotNull @Size(min = 4, max = 27962028) @Base64String String messageData,
+        // 12 bytes IV is 16 bytes in B64
+        @NotNull @Size(min = 16, max = 16) @Base64String String messageIV
 ) {}
