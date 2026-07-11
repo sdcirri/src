4d2ea6e3bc956731ee4cc3e0251d022b8ddccba5
sdcirri <sebadeod@proton.me>
2026-07-11 14:22:07 +0200

    nullability

--- src/main/java/it/sdc/src/validation/Base64StringValidator.java
+++ src/main/java/it/sdc/src/validation/Base64StringValidator.java
@@ -8,7 +8,8 @@ import java.util.Base64;
 public class Base64StringValidator implements ConstraintValidator<Base64String, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext ctx) {
-        if (value == null) return false;
+        // nullability is out of validator's concern
+        if (value == null) return true;
         try {
             Base64.getDecoder().decode(value);
             return true;
