37b32d8c72e055390675ae0ebf9dfc8bc8e5a72f
Sebastiano Deodati <sebastiano.deodati@etravelpartner.it>
2026-07-13 12:49:08 +0200

    unused import

--- src/main/java/it/sdc/src/exceptions/GlobalExceptionHandler.java
+++ src/main/java/it/sdc/src/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package it.sdc.src.exceptions;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.server.resource.introspection.BadOpaqueTokenException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
