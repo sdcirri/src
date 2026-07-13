959a1e17105756d7841a10358254492d9ac336b7
Sebastiano Deodati <sebastiano.deodati@etravelpartner.it>
2026-07-13 14:22:56 +0200

    newline

--- src/test/java/it/sdc/src/controllers/UserControllerTest.java
+++ src/test/java/it/sdc/src/controllers/UserControllerTest.java
@@ -20,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
     @Mock
