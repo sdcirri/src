74fa80fe0e0068937e8cea4f6d7ef884d8bd3b19
Sebastiano Deodati <sebastiano.deodati@etravelpartner.it>
2026-09-03 14:49:43 +0200

    types

---
+++ webui/src-frontend/src/api/types.ts
@@ -0,0 +1,43 @@
+export type MessageDto = {
+    timestamp: number;
+    data: string;
+    iv: string;
+    direction: 'INCOMING' | 'OUTGOING';
+};
+
+export type ChatDto = {
+    chatId: string;
+    contactId: string;
+    lastMessage: MessageDto;
+};
+
+export type ContactCryptoDto = {
+    publicEd25519: string;
+    publicX25519: string;
+};
+
+export type UserCryptoDto = {
+    id: string;
+    kekSalt: string;
+    privateEd25519Crypto: string;
+    privateEd25519IV: string;
+    publicEd25519: string;
+    privateX25519Crypto: string;
+    privateX25519IV: string;
+    publicX25519: string;
+};
+
+export type UserDto = {
+    id: string;
+    username: string;
+    displayName: string;
+    proPic: string;
+};
+
+export type UserSessionDto = {
+    id: string;
+    accessToken: string;
+    accessTokenExpires: number;
+    refreshToken: string;
+    refreshTokenExpires: number;
+};
