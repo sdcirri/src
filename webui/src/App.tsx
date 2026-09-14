86815326c72ddd0f7c5e06f6c18e6fb07acf2c09
Sebastiano Deodati <sebastiano.deodati@etravelpartner.it>
2026-09-14 11:25:17 +0200

    main screen prototpying

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,10 +1,15 @@
+import '@/css/main.css';
 
 function App() {
-  return (
-    <>
-      <h1>The coolest GUI ever</h1>
-    </>
-  )
+    return (
+        <div id='root-container'>
+            <div id='top-bar'></div>
+            <div id='app-container'>
+                <div id='sidebar'></div>
+                <div id='chat-container'></div>
+            </div>
+        </div>
+    )
 }
 
 export default App
