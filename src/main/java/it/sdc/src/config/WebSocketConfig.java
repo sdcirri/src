fec2f45a9a6f22234b5f7f4877aafeb3d0bc8a80
sdcirri <sebadeod@proton.me>
2026-09-26 16:56:25 +0200

    space

--- src/main/java/it/sdc/src/config/WebSocketConfig.java
+++ src/main/java/it/sdc/src/config/WebSocketConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final AppCorsProperties appCorsProperties;
 
     @Override
